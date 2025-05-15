b3532cd8130290bc37a21ba3f17230f0726f2ee7
Dennis <dennis7876@hotmail.com>
2025-05-15 02:44:49 +0200

    Final

---
+++ script.js
@@ -0,0 +1,36 @@
+const CreateButton = document.getElementById('b1') // Button button
+
+CreateButton.addEventListener("click", newButtonEvent)
+
+let number = 1;
+
+function newButtonEvent(){
+    const newButton = document.createElement('button');
+    newButton.textContent = 'button '+number;
+    newButton.className = 'game-button';
+
+    newButton.addEventListener('click', function () {   //Själv Ta bort funtion
+        newButton.remove();
+    });
+
+    document.getElementById('stackpanel').appendChild(newButton);
+    number++;
+}   
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+const rotatebutton = document.getElementById('b2but') // SPIN BUTTON
+
+rotatebutton.addEventListener("click", Rotate) 
+
+function Rotate(){
+    const button1 = document.getElementById('b1')
+    const button3 = document.getElementById('b3')
+
+    let currentRotation1 = parseInt(button1.style.transform.replace('rotate(', '').replace('deg)', '')) || 0;
+    let currentRotation3 = parseInt(button3.style.transform.replace('rotate(', '').replace('deg)', '')) || 0;
+
+    button1.style.transform = `rotate(${currentRotation1 + 45}deg)`;
+    button3.style.transform = `rotate(${currentRotation3 - 45}deg)`;
+}   
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
